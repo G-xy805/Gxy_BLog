e1a03ec05654df1bde1a19e1a47bbdbb654d54aa
zayck <savaii@163.com>
2026-01-19 09:32:14 +0800

    Kael 优化修复版

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,16 +1,14 @@
 import type { APIRoute } from "astro";
 
-const robotsTxt = `
+function getRobotsTxt(sitemapURL: URL) {
+  return `
 User-agent: *
-Disallow: /_astro/
+Allow: /
+Sitemap: ${sitemapURL.href}
+`;
+}
 
-Sitemap: ${new URL("sitemap-index.xml", import.meta.env.SITE).href}
-`.trim();
-
-export const GET: APIRoute = () => {
-	return new Response(robotsTxt, {
-		headers: {
-			"Content-Type": "text/plain; charset=utf-8",
-		},
-	});
+export const GET: APIRoute = ({ site }) => {
+  const sitemapURL = new URL("sitemap-index.xml", site);
+  return new Response(getRobotsTxt(sitemapURL));
 };
